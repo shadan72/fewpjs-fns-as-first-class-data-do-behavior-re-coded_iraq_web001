5d69eae7eed362eeeb82ce0cece5159698ab6df0
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-05-15 11:56:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 function greet(hm){
   const con=parseInt(hm)
-  const str=document.getElementById('')
-  if(con < 12)
-  
+  const str=document.getElementById('greeting')
+  if(con < 12){
+    str.innerHTML='Good Morning'
+  }else if(con <=12 && con>=5) {
+    str.innerHTML='Good Afternoon'
+  }else if (con <5)
+  str.innerHTML='Good Evening'
 }
 /* Write your implementation of displayMessage() */
